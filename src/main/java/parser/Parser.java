85f0d3cd58c4b0c6d7c2bc339e6898dae1c6102a
SinaBolouki <sina.bolouki@gmail.com>
2021-12-28 11:14:28 +0330

    made lexicalAnalyzer local

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,6 +1,12 @@
 package parser;
 
 
+import Log.Log;
+import codegenerator.CodeGenerator;
+import errorHandler.ErrorHandler;
+import scanner.LexicalAnalyzer;
+import scanner.token.Token;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,85 +14,78 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-import Log.Log;
-import codegenerator.CodeGenerator;
-import errorHandler.ErrorHandler;
-import scanner.LexicalAnalyzer;
-import scanner.token.Token;
-
 
 public class Parser {
-  private List<Rule> rules;
-  private Stack<Integer> parsStack;
-  private ParseTable parseTable;
-  private LexicalAnalyzer lexicalAnalyzer;
-  private CodeGenerator cg;
+    private final List<Rule> rules;
+    private final Stack<Integer> parsStack;
+    private ParseTable parseTable;
+    private final CodeGenerator cg;
 
-  public Parser() {
-    parsStack = new Stack<>();
-    parsStack.push(0);
-    try {
-      parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    rules = new ArrayList<>();
-    try {
-      for (String stringRule : Files.readAllLines(Paths.get("src/main/resources/Rules"))) {
-        rules.add(new Rule(stringRule));
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    cg = new CodeGenerator();
-  }
-
-  public void startParse(java.util.Scanner sc) {
-    lexicalAnalyzer = new LexicalAnalyzer(sc);
-    Token lookAhead = lexicalAnalyzer.getNextToken();
-    boolean finish = false;
-    Action currentAction;
-    while (!finish) {
-      try {
-        Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
-//                Log.print("state : "+ parsStack.peek());
-        currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
-        Log.print(currentAction.toString());
-        //Log.print("");
-
-        switch (currentAction.action) {
-          case shift:
-            parsStack.push(currentAction.number);
-            lookAhead = lexicalAnalyzer.getNextToken();
-
-            break;
-          case reduce:
-            Rule rule = rules.get(currentAction.number);
-            for (int i = 0; i < rule.rHS.size(); i++) {
-              parsStack.pop();
-            }
-
-            Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.lHS);
-//                        Log.print("LHS : "+rule.LHS);
-            parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.lHS));
-            Log.print(/*"new State : " + */parsStack.peek() + "");
-//                        Log.print("");
-            try {
-              cg.semanticFunction(rule.semanticAction, lookAhead);
-            } catch (Exception e) {
-              Log.print("Code Genetator Error");
-            }
-            break;
-          case accept:
-            finish = true;
-            break;
-          default:
+    public Parser() {
+        parsStack = new Stack<>();
+        parsStack.push(0);
+        try {
+            parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Log.print("");
+        rules = new ArrayList<>();
+        try {
+            for (String stringRule : Files.readAllLines(Paths.get("src/main/resources/Rules"))) {
+                rules.add(new Rule(stringRule));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cg = new CodeGenerator();
+    }
 
-      } catch (Exception e) {
+    public void startParse(java.util.Scanner sc) {
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(sc);
+        Token lookAhead = lexicalAnalyzer.getNextToken();
+        boolean finish = false;
+        Action currentAction;
+        while (!finish) {
+            try {
+                Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
+//                Log.print("state : "+ parsStack.peek());
+                currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
+                Log.print(currentAction.toString());
+                //Log.print("");
 
-        e.printStackTrace();
+                switch (currentAction.action) {
+                    case shift:
+                        parsStack.push(currentAction.number);
+                        lookAhead = lexicalAnalyzer.getNextToken();
+
+                        break;
+                    case reduce:
+                        Rule rule = rules.get(currentAction.number);
+                        for (int i = 0; i < rule.rHS.size(); i++) {
+                            parsStack.pop();
+                        }
+
+                        Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.lHS);
+//                        Log.print("LHS : "+rule.LHS);
+                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.lHS));
+                        Log.print(/*"new State : " + */parsStack.peek() + "");
+//                        Log.print("");
+                        try {
+                            cg.semanticFunction(rule.semanticAction, lookAhead);
+                        } catch (Exception e) {
+                            Log.print("Code Genetator Error");
+                        }
+                        break;
+                    case accept:
+                        finish = true;
+                        break;
+                    default:
+                }
+                Log.print("");
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
 //                boolean find = false;
 //                for (NonTerminal t : NonTerminal.values()) {
 //                    if (parseTable.getGotoTable(parsStack.peek(), t) != -1) {
@@ -102,16 +101,16 @@ public class Parser {
 //                }
 //                if (!find)
 //                    parsStack.pop();
-      }
+            }
+
+
+        }
+        if (!ErrorHandler.hasError) {
+            cg.printMemory();
+        }
 
 
     }
-    if (!ErrorHandler.hasError) {
-        cg.printMemory();
-    }
-
-
-  }
 
 
 }
